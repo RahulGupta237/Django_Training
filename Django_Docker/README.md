4ca7ce472a3ca2260d3c6ced41e30b670b26dc74
Rahul gupta <rahhulazm206@gmail.com>
2021-10-12 11:49:56 +0530

    Delete Django Docker

--- Django_Docker/README.md
+++
@@ -1,2 +0,0 @@
-# YT-Django-Docker-Compose-Introduction
- 
--- Django_Docker/WebApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
-def Compose_docker(request):
-    dict_docker={"Create_project":"docker-compose run web django-admin startproject Kellton .",
-                 "Create_App":" docker exec docker_app_web_1 django-admin startapp WebApp",}
-    #return HttpResponse("<h1>Hellow Compose_docker</h1>")
-    return render(request,"WebApp/Docker.html",dict_docker)
